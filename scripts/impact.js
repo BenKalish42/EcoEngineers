bc5ab49414126e0c1b5ba1701a2790e39f602b50
Benjamin Kalish <bkalish@rocketvisor.com>
2018-05-23 21:21:37 -0500

    added the Making an Impact page

---
+++ scripts/impact.js
@@ -0,0 +1,97 @@
+function main() {
+
+	var app = new Vue({
+		el: "#impactPage",
+		data: {
+			amount: 0,
+			percent: 0,
+			calculated: false,
+		}
+	});
+
+	const epy = 692.0
+
+	$("#calculateForm").submit(function(event){
+
+		event.preventDefault();
+
+		var num_people = $("#numPeople").val();
+		var num_years = $("#numYears").val();
+
+		var item_values = {
+			cans: 89.38,
+			plastic: 35.56,
+			glass: 25.39,
+			newspaper: 113.14,
+			magazines: 27.46,
+		}
+
+		for (var el of $('#recyclableItems').find('input')){
+			if (!el.checked){
+				item_values[$(el).val()] = 0;
+			}
+		}
+
+		// calculate emissions data
+		var normal_total = num_people*num_years*epy;
+		var emissions_reduction = objectSum(item_values);
+		var calculated_total = num_people*num_years*(epy - emissions_reduction);
+
+		// set Vue variables
+		app.amount = truncateDecimals(normal_total - calculated_total);
+		app.percent = truncateDecimals((1-(calculated_total/normal_total))*100.0);
+		app.calculated = true;
+
+
+
+		// Plotly Graph
+
+		var trace1 = {
+		  x: [],
+		  y: [],
+		  type: 'scatter',
+		  name: 'Average Emissions'
+		};
+
+		var trace2 = {
+		  x: [],
+		  y: [],
+		  type: 'scatter',
+		  name: 'Your Emissions'
+		};
+
+		for (var i = 0; i <= num_years; i++){
+			trace1["x"][i] = i
+			trace1["y"][i] = i*num_people*epy
+
+			trace2["x"][i] = i
+			trace2["y"][i] = i*num_people*(epy-emissions_reduction)
+		}
+
+		var data = [trace1, trace2];
+
+		var layout = {
+		  title:'Your Emissions At Northwestern'
+		};
+
+		Plotly.newPlot('emissionsChart', data, layout);
+
+	});
+
+}
+
+function objectSum(obj) {
+  var sum = 0;
+  for( var el in obj ) {
+    if( obj.hasOwnProperty( el ) ) {
+      sum += parseFloat( obj[el] );
+    }
+  }
+  return sum;
+}
+
+function truncateDecimals (number) {
+    return Math[number < 0 ? 'ceil' : 'floor'](number);
+};
+
+main();
--- scripts/item.js
+++ scripts/item.js
@@ -14,6 +14,7 @@ function main(items_data) {
 var items_data = {
 	"can": {
 		"name": "Aluminum Cans",
+		"recyclable": true,
 		"where": "Aluminum cans can be recycled in any regular blue-colored recycling bin.",
 		"facts": "Americans throw away over $1 billion worth of aluminum cans every year. Discarded aluminum is more valuable than any other item in a recycling bin. Aluminum cans are 100% recyclable."
 	}
